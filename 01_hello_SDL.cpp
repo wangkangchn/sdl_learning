c81ca6b13851f76a5cbd9a1ffe1a3f443f0ac8a0
wkangk <wangkangchn@163.com>
2023-03-23 22:39:25 +0800

    口建

--- 01_hello_SDL.cpp
+++ 01_hello_SDL.cpp
@@ -4,53 +4,79 @@ and may not be redistributed without written permission.*/
 //Using SDL and standard IO
 #include <SDL2/SDL.h>
 #include <stdio.h>
-
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+#include <ctime>
+#include <random>
+//Screen dimension constants	屏幕大小
+const int SCREEN_WIDTH = 888;
+const int SCREEN_HEIGHT = 888;
 
 int main( int argc, char* args[] )
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-	
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+    bool quit{false}; 
+    int gap = 2;
+    std::default_random_engine engine;
+    std::uniform_int_distribution<uint8_t> u(0, 255); // 左闭右闭区间
+    engine.seed(time(0));
 
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
-		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface( window );
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+    
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;  /* 就是一个二维图像, 这个图像可以从文件读取也可以从窗口中获取 */
 
-			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
-            
-            //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-		}
-	}
+    //Initialize SDL
+    Uint32 subsystem_init = SDL_Init( SDL_INIT_VIDEO  ); 
+    if ( subsystem_init < 0 ) {  /* 所有 SDL 的方法必须在初始化之后才能使用 */
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        goto ret;
+    }
 
-	//Destroy window
-	SDL_DestroyWindow( window );
+    if (subsystem_init & SDL_INIT_VIDEO) {
+        printf("Video is initialized.\n"); 
+    } else {
+        printf("Video is not initialized.\n");
+    }
 
-	//Quit SDL subsystems
-	SDL_Quit();
+    printf("初始化标记: %d\n", subsystem_init);
+    // SDL_WasInit(SDL_INIT_VIDEO)
 
-	return 0;
+    //Create window
+    window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if( window == NULL ) {
+        printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        goto ret;
+    }
+
+    //Get window surface
+    screenSurface = SDL_GetWindowSurface( window );
+
+    //Fill the surface white
+
+    for (size_t row = 0; row < SCREEN_HEIGHT; ++row) {
+        SDL_Rect r = {0, row, SCREEN_WIDTH, gap};
+        SDL_FillRect( screenSurface, &r, SDL_MapRGB( screenSurface->format, u(engine), u(engine), u(engine) ) );
+    }
+    
+    //Update the surface 
+    SDL_UpdateWindowSurface( window );  /* 这一个才是将绘制的画布放到窗口上显示 */
+    
+    //Hack to get window to stay up
+    SDL_Event e; 
+    
+    while ( !quit ) { 
+        while( SDL_PollEvent( &e ) ) { 
+            if ( e.type == SDL_QUIT ) {
+                quit = true; 
+            }
+        } 
+    }
+
+ret:    /* goto 后不能有任何声明!!! */
+    //Destroy window
+    SDL_DestroyWindow( window );
+
+    //Quit SDL subsystems
+    SDL_Quit();
+
+    return 0;
 }
